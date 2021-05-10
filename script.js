5416c76b6a02e4d9580b8e9ee56a26c701f95ac9
Monish Singhal <monishsinghal7@gmail.com>
2021-05-10 17:54:08 +0530

    Added credits and source commands

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(function() {
     $('body').terminal({
-        hello: function(what) {
+            hello: function(what) {
             this.echo('Hello, ' + what + '. Welcome to this terminal.');
+        }, credits: function() {
+            this.echo("This website would not have been possible without the wonderful [[ub;;;;]JQuery Terminal Package], which can be found [[!;;;;https://github.com/jcubic/jquery.terminal]here]")
+        }, source: function() {
+            this.echo("Source code for the website can be found  [[!;;;;https://github.com/mynameismon/mynameismon]here].")
         }
     }, {
-    greetings: "Welcome to my website! Clearly, there is a lot of work to do :D"
+    greetings: "Welcome to my website! Clearly, there is a lot of work to do :D \n"
     })
 });
-
