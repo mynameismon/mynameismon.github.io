467d44590aaf3d61af55a0a056e06934a351fcdd
Monish Singhal <monishsinghal7@gmail.com>
2021-05-11 16:56:39 +0530

    Add custom error message.
    Added a custom error message, which is randomized. Although there aren't
    too many options as of now, more will be added moving forward.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var commandText = function (text, colour) {
+    return '[[gb;' + color + ';]' + text + ']'
+};
+
+var errorText = function (text) {
+    return '[[g;red;]' + text + ']'
+};
+
 $(function() {
     $('body').terminal({
             hello: function(what) {
@@ -8,6 +16,17 @@ $(function() {
             this.echo("Source code for the website can be found  [[!;;;;https://github.com/mynameismon/mynameismon]here].")
         }
     }, {
-    greetings: "Welcome to my website! Clearly, there is a lot of work to do :D \n"
-    })
+    greetings: "Welcome to my website! Clearly, there is a lot of work to do :D \n",
+    onCommandNotFound: function(cmd, term) {
+        error_strings = [ errorText("Whoops! Looks like " + cmd + " is not found! Try running ") + commandText("help", "green") + errorText(", maybe?"),
+                    errorText("Are you sure " + cmd + "is right? :( Maybe you can check the spelling or type ") + commandText("help", "green") + errorText(", if that helps.")]
+        term.echo(error_strings[Math.floor(error_strings.length * Math.random())])
+    }
+    }) 
 });
+
+/** TODO:
+    1. add help
+    2. add special effects
+    3. refer to https://nntoan.com and http://tnt2113.github.io/ for inspiration
+ */
